87a85a2398d892d65370f9cf34630331a67acf39
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-11-20 10:46:55 +0100

    add: metadata for 0.1 release

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from distutils.core import setup
 
+version = open('VERSION').read().strip()
+
 setup(
     name="Autocurry",
-    version=open('VERSION').read().strip(),
-    author='Felix Bruckmeier',
-    author_email='felix.m.bruckmeier@gmail.com',
-
+    packages=['autocurry'],
+    version=version,
     description='Automatic currying using a decorator',
     long_description=open('README.md').read(),
+    author='Felix Bruckmeier',
+    author_email='felix.m.bruckmeier@gmail.com',
     url='https://github.com/felixbr/autocurry',
+    download_url='https://github.com/felixbr/autocurry/tarball/%s' % version,
+    keywords=['functional', 'currying'],
     license='BSD',
-
-    packages=['autocurry']
 )
