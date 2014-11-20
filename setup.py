cd789379445ff003195ad51af96c6fcac8a61e10
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-11-20 12:44:07 +0100

    add: improved setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+from io import open
 
-version = open('VERSION').read().strip()
+version = open('VERSION', 'r', encoding='utf-8').read().strip()
 
 setup(
-    name="Autocurry",
-    packages=['autocurry'],
-    version=version,
-    description='Automatic currying using a decorator',
-    long_description=open('README.md').read(),
-    author='Felix Bruckmeier',
-    author_email='felix.m.bruckmeier@gmail.com',
-    url='https://github.com/felixbr/autocurry',
-    download_url='https://github.com/felixbr/autocurry/tarball/%s' % version,
-    keywords=['functional', 'currying'],
-    license='BSD',
+    name = "Autocurry",
+    packages = ['autocurry'],
+    version = version,
+    description = 'Automatic currying using a decorator',
+    long_description = open('README.md', 'r', encoding='utf-8').read(),
+    author = 'Felix Bruckmeier',
+    author_email = 'felix.m.bruckmeier@gmail.com',
+    url = 'https://github.com/felixbr/autocurry',
+    download_url = 'https://github.com/felixbr/autocurry/tarball/%s' % version,
+    keywords = ['functional', 'currying'],
+    license = 'BSD',
+    install_requires = []
 )
